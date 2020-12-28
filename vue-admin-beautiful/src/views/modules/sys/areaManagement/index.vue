0d710c70c0122de86f4268a0dde99c0b0e2df37a
Parker <meet.parker@foxmail.com>
2020-12-28 17:55:34 +0800

    地域设置

--- vue-admin-beautiful/src/views/modules/sys/areaManagement/index.vue
+++ vue-admin-beautiful/src/views/modules/sys/areaManagement/index.vue
@@ -83,8 +83,8 @@
       return {
         dict: {
         },
-        // 0 世界级  ---- 86 中国
-        defaultNode: "86",
+        // 0 世界级  ---- 1343492673444646912 中国
+        defaultNode: "1343492673444646912",
         data: [],
         tmpTreeData: {},
         defaultProps: {
