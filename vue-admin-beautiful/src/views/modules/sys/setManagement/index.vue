9bf8fd5bbadcb2fe4082d053cdf93ad62cda3109
hiparker <meet.parker@foxmail.com>
2021-04-29 19:47:21 +0800

    SMTP邮件服务在线配置化

--- vue-admin-beautiful/src/views/modules/sys/setManagement/index.vue
+++ vue-admin-beautiful/src/views/modules/sys/setManagement/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div v-loading="loadingData" class="setManagement-container">
     <el-tabs v-model="activeName" v-loading="loadingTabs">
-      <el-tab-pane label="系统默认" name="def">
+      <el-tab-pane name="def">
+        <span slot="label"><i class="el-icon-c-scale-to-original"></i> 系统默认</span>
         <el-form
           ref="defForm"
           :model="def.form"
@@ -41,7 +42,8 @@
         </el-form>
       </el-tab-pane>
 
-      <el-tab-pane label="接口加密" name="crypto">
+      <el-tab-pane name="crypto">
+        <span slot="label"><i class="el-icon-key"></i> 接口加密</span>
         <el-form
           ref="cryptoForm"
           :model="crypto.form"
@@ -134,6 +136,105 @@
           </el-button>
         </el-form>
       </el-tab-pane>
+
+
+      <el-tab-pane name="email">
+        <span slot="label"><i class="el-icon-message"></i> SMTP服务</span>
+        <el-form
+          ref="emailForm"
+          :model="email.form"
+          :rules="email.rules"
+          label-width="125px"
+        >
+
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="SMTP地址" prop="email_smtp">
+                <el-autocomplete
+                  class="inline-input"
+                  v-model="email.form.email_smtp"
+                  :fetch-suggestions="smtpQuerySearch"
+                  placeholder="请输入SMTP地址"
+                  style="width: 100%"
+                ></el-autocomplete>
+              </el-form-item>
+            </el-col>
+          </el-row>
+
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="SMTP端口" prop="email_port">
+                <el-input
+                  v-model="email.form.email_port"
+                  autocomplete="off"
+                ></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="开启SSL认证" prop="email_ssl_enable">
+                <el-select
+                  v-model="email.form.email_ssl_enable"
+                  placeholder="请选择是否开启SSL认证" >
+                  <el-option
+                    v-for="item in dict.no_yes"
+                    :key="item.dictValue"
+                    :label="item.dictName"
+                    :value="item.dictValue"
+                  ></el-option>
+                </el-select>
+              </el-form-item>
+            </el-col>
+          </el-row>
+
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="邮箱账号" prop="email_account">
+                <el-input
+                  v-model="email.form.email_account"
+                  autocomplete="off"
+                  placeholder="xxxx@xx.com"
+                ></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="邮箱账号密码" prop="email_password">
+                <el-input
+                  v-model="email.form.email_password"
+                  autocomplete="off"
+                  placeholder="某些邮箱需要为SMTP服务单独设置密码"
+                ></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="发件人" prop="email_addresser">
+                <el-input
+                  v-model="email.form.email_addresser"
+                  autocomplete="off"
+                  placeholder="发件人 xxxx@xx.com 或 测试<xxx.xx.com>"
+                ></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+
+          <el-button
+            type="primary"
+            style="margin-top: 50px"
+            @click="save('emailForm', email.form)"
+          >
+            保存
+          </el-button>
+        </el-form>
+      </el-tab-pane>
+
     </el-tabs>
   </div>
 </template>
@@ -146,6 +247,7 @@
     doUpdateOptions,
   } from "@/api/set/setManagement";
   import { isNull } from "@/utils/validate";
+  import {getMsg, isNumber} from "@/utils/valiargs";
 
 
   export default {
@@ -153,6 +255,15 @@
     components: {
     },
     data() {
+
+      const validate_optionCode_isNumber = (rule, value, callback) => {
+        if (!isNumber(value)) {
+          callback(new Error(getMsg("isNumber")));
+        } else {
+          callback();
+        }
+      };
+
       return {
         activeName: "def",
         dict: {},
@@ -191,6 +302,51 @@
           },
         },
 
+        email: {
+          form: {
+            email_smtp: "",
+            email_port: "",
+            email_ssl_enable: "",
+            email_account: "",
+            email_password: "",
+            email_addresser: "",
+          },
+          rules: {
+            email_smtp: [
+              { required: true, trigger: "change", message: "请输入SMTP地址" },
+            ],
+            email_port: [
+              { required: true, trigger: "blur", message: "请输入SMTP端口" },
+              { required: false, trigger: "blur", validator: validate_optionCode_isNumber },
+            ],
+            email_account: [
+              { required: true, trigger: "blur", message: "请输入邮箱账号" },
+            ],
+            email_password: [
+              { required: true, trigger: "blur", message: "请输入邮箱密码" },
+            ],
+            email_addresser: [
+              { required: true, trigger: "blur", message: "请输入发件人" },
+            ]
+          },
+        },
+        smtpRestaurants: [
+          { value: "smtp.aliyun.com"},
+          { value: "smtp.gmail.com"},
+          { value: "smtp.sina.com.cn"},
+          { value: "smtp.tom.com"},
+          { value: "smtp.163.com"},
+          { value: "smtp.126.com"},
+          { value: "smtp.mail.yahoo.com"},
+          { value: "smtp.mail.yahoo.com.cn"},
+          { value: "smtp.foxmail.com"},
+          { value: "smtp.foxmail.com.cn"},
+          { value: "smtp.qq.com"},
+          { value: "smtp.sohu.com"},
+          { value: "smtp.live.com"},
+          { value: "smtp.139.com"},
+          { value: "smtp.china.com"},
+        ],
         loadingData: true,
         loadingTabs: false,
       };
@@ -244,6 +400,19 @@
 
       // ================================
 
+      smtpQuerySearch(queryString, cb) {
+        let restaurants = this.smtpRestaurants;
+        let results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants;
+        // 调用 callback 返回建议列表的数据
+        cb(results);
+      },
+
+      createFilter(queryString) {
+        return (restaurant) => {
+          return (restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+        };
+      },
+
       // 复制
       handleClipboard(text, event) {
         clipboard(text, event);
@@ -268,6 +437,24 @@
             this.baseData.crypto_asymmetric_public_key.optionValue:"";
           this.crypto.form.crypto_asymmetric_private_key = !isNull(this.baseData.crypto_asymmetric_private_key)?
             this.baseData.crypto_asymmetric_private_key.optionValue:"";
+
+
+
+          // 邮箱配置
+          this.email.form.email_smtp = !isNull(this.baseData.email_smtp)?
+            this.baseData.email_smtp.optionValue:"";
+          this.email.form.email_port = !isNull(this.baseData.email_port)?
+            this.baseData.email_port.optionValue:"";
+          this.email.form.email_ssl_enable = !isNull(this.baseData.email_ssl_enable)?
+            this.baseData.email_ssl_enable.optionValue:"1";
+          this.email.form.email_account = !isNull(this.baseData.email_account)?
+            this.baseData.email_account.optionValue:"";
+          this.email.form.email_password = !isNull(this.baseData.email_password)?
+            this.baseData.email_password.optionValue:"";
+          this.email.form.email_addresser = !isNull(this.baseData.email_addresser)?
+            this.baseData.email_addresser.optionValue:"";
+
+
 
           setTimeout(() => {
             this.loadingData = false;
