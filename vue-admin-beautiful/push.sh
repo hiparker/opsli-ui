aa337c321caebe241d0464c3599b45953e250c34
Parker <meet.parker@foxmail.com>
2021-05-11 00:09:25 +0800

    git 提交脚本

--- vue-admin-beautiful/push.sh
+++ vue-admin-beautiful/push.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-set -e
-git init
-git add -A
-git commit -m 'deploy'
-git push -f "https://${access_token}@github.com/chuzhixin/vue-admin-beautiful.git" master
-exec /bin/bash
+git push "git@github.com:hiparker/opsli-ui.git" master
+git push -f "git@gitee.com:hiparker/opsli-ui.git" master
 
 
 
